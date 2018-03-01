f435a71679edd20a78426df266abee411f8ad0ea
Samuel <sreddic3@uncc.edu>
2018-03-01 01:49:02 +0000

    Section 4(CSS)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,12 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
   
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
+  end
+  
   # add below all other methods
 private
 
